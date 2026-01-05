7a0f814d0dde473c8af67b83696ef1d5f84ce07d
loucas.kreger <loucas.kreger@moviri.com>
2026-01-05 14:52:44 -0500

    Multithread metric collection

--- src/main/java/com/moviri/plugins/Dynatrace.java
+++ src/main/java/com/moviri/plugins/Dynatrace.java
@@ -20,6 +20,7 @@ import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.LogManager;
@@ -29,7 +30,7 @@ import java.util.stream.Collectors;
 public class Dynatrace extends PeriodicWork {
 
     private static final Logger DT_LOGGER = LoggerFactory.getLogger(Dynatrace.class);
-    private static final String VERSION = "1.0.3";
+    private static final String VERSION = "1.0.4";
     private static final long RECURRENCE_PERIOD = TimeUnit.MINUTES.toMillis(1);
     public static final String LOG_PACKAGE_NAME = "com.moviri.plugins";
     public static final String LOG_RECORDER_NAME = "Dynatrace logs";
@@ -102,6 +103,27 @@ public class Dynatrace extends PeriodicWork {
         return RECURRENCE_PERIOD;
     }
 
+    private void collectFSMetrics(DynatraceClient client) {
+        if (DynatraceConfiguration.get().isFilesystemMetricsEnabled()) {
+            DT_LOGGER.info("Collecting Filesystem Metrics");
+            client.postMintMetrics(new FilesystemMetricCollector().collect());
+        }
+    }
+
+    private void collectExecutorMetrics(DynatraceClient client) {
+        if (DynatraceConfiguration.get().isExecutorMetricsEnabled()) {
+            DT_LOGGER.info("Collecting Executor Metrics");
+            client.postMintMetrics(new ExecutorCollector().collect());
+        }
+    }
+
+    private void collectJobLogs(DynatraceClient client) {
+        if (DynatraceConfiguration.get().isJobLogEnabled()) {
+            DT_LOGGER.info("Collecting Job Logs");
+            client.postLogLines(new JobLogCollector().collect());
+        }
+    }
+
     @Override
     protected void doRun() {
         DT_LOGGER.info("Running query method ({}) ({})", Jenkins.getVersion(), VERSION);
@@ -114,19 +136,10 @@ public class Dynatrace extends PeriodicWork {
 
         try {
             DynatraceClient client = getDynatraceClient();
-            if (DynatraceConfiguration.get().isFilesystemMetricsEnabled()) {
-                DT_LOGGER.info("Collecting Filesystem Metrics");
-                client.postMintMetrics(new FilesystemMetricCollector().collect());
-            }
-            if (DynatraceConfiguration.get().isExecutorMetricsEnabled()) {
-                DT_LOGGER.info("Collecting Executor Metrics");
-                client.postMintMetrics(new ExecutorCollector().collect());
-            }
-
-            if (DynatraceConfiguration.get().isJobLogEnabled()) {
-                DT_LOGGER.info("Collecting Job Logs");
-                client.postLogLines(new JobLogCollector().collect());
-            }
+            CompletableFuture<Void> fsMetrics = CompletableFuture.runAsync(() -> collectFSMetrics(client));
+            CompletableFuture<Void> executorMetrics = CompletableFuture.runAsync(() -> collectExecutorMetrics(client));
+            CompletableFuture<Void> jobLogs = CompletableFuture.runAsync(() -> collectJobLogs(client));
+            CompletableFuture.allOf(fsMetrics, executorMetrics, jobLogs).join();
         } catch (Exception e) {
             DT_LOGGER.error(e.toString());
             e.printStackTrace();
